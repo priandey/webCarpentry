1e70b2b04792a6087c83281e9b324a7b5426ac46
Pierre Riandey <42967943+priandey@users.noreply.github.com>
2024-06-15 13:18:39 +0200

    Update settings.py

--- webCarpentry/webCarpentry/settings.py
+++ webCarpentry/webCarpentry/settings.py
@@ -125,12 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.getenv("STATIC_URL", '/static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = False
