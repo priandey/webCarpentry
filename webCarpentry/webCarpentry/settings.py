a72b821cc2ec3c325289c27d368f4f5b7d340cd8
Pierre Riandey <42967943+priandey@users.noreply.github.com>
2024-06-15 12:16:58 +0200

    Update settings.py

--- webCarpentry/webCarpentry/settings.py
+++ webCarpentry/webCarpentry/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
